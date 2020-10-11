95e638f652f4b91a77ff4633da1bda2f8ebbaafd
machamp0714 <rateliasudaisuki@gmail.com>
2020-10-11 22:21:23 +0900

    install react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,103 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing';
-
-interface SelectedImage {
-  localUri: string
-}
+import React from 'react';
+import { Header } from 'react-native-elements';
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = useState<SelectedImage | null>(null);
-
-  const openImagePickerAsync = async () => {
-    const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    const pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    setSelectedImage({ localUri: pickerResult.uri })
-  }
-
-  const openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert("Uh oh, sharing isn't available on your platform");
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage?.localUri!);
-  }
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
-        <TouchableOpacity
-          onPress={openShareDialogAsync}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
-
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
-
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
-      <StatusBar style="auto" />
-
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-    </View>
+    <Header
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'Header', style: { color: '#fff' } }}
+      rightComponent={{ icon: 'home', color: '#fff' }}
+    />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 30
-  },
-  instructions: {
-    color: '#888',
-    fontSize: 18,
-    marginHorizontal: 15
-  },
-  button: {
-    backgroundColor: 'blue',
-    padding: 20,
-    borderRadius: 5,
-    marginTop: 20
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff'
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain'
-  }
-});
