c2ce677f51d9213ee73c3203c492b854e3f90eb5
machamp0714 <rateliasudaisuki@gmail.com>
2020-10-18 21:04:16 +0900

    メモ追加画面を作成

--- src/screens/AddTodo.tsx
+++ src/screens/AddTodo.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { FC, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 const AddTodo: FC = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>ToDo作成画面です</Text>
-    </View>
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <Input placeholder='タイトルを入力してください' value={title} onChangeText={(text) => setTitle(text)} />
+      <Input placeholder='メモを入力してください' value={content} onChangeText={(text) => setContent(text)} />
+      <Button title='追加' style={{ width: 100 }} />
+    </KeyboardAvoidingView>
   )
 }
 
@@ -13,7 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: 20
   }
 })
 
