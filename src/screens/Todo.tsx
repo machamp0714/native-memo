0cf78b91ff4c01e8c0f3c66217de6b337d03e8b3
machamp0714 <rateliasudaisuki@gmail.com>
2020-10-24 22:13:31 +0900

    更新ボタンと削除ボタンを設置

--- src/screens/Todo.tsx
+++ src/screens/Todo.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
-import { Card } from 'react-native-elements';
-import { StyleSheet, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import { StyleSheet, View, Text } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { ToDoStackParamList } from '../navigators/ToDoNavigator';
 import format from 'date-fns/format';
@@ -11,7 +11,7 @@ export interface ToDo {
   createdAt: number
 }
 
-type Props = {
+interface Props {
   route: RouteProp<ToDoStackParamList, 'ToDo'>
 }
 
@@ -19,16 +19,30 @@ const Todo: FC<Props> = ({ route }) => {
   const { title, content, createdAt } = route.params.todo;
 
   return (
-    <Card>
-      <Card.Title>{title}</Card.Title>
-      <Card.Divider />
-      <Text>{content}</Text>
-      <Text style={styles.createdAt}>作成日時: {format(createdAt, 'yyyy.MM.dd HH:mm')}</Text>
-    </Card>
+    <View style={styles.container}>
+      <Card>
+        <Card.Title>{title}</Card.Title>
+        <Card.Divider />
+        <Text>{content}</Text>
+        <Text style={styles.createdAt}>作成日時: {format(createdAt, 'yyyy.MM.dd HH:mm')}</Text>
+      </Card>
+      <View style={styles.buttonGroup}>
+        <Button title='更新' />
+        <Button title='削除' buttonStyle={{ backgroundColor: 'red', marginLeft: 10 }} />
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column'
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    marginHorizontal: 15,
+    marginTop: 10
+  },
   createdAt: {
     color: '#999',
     marginTop: 20,
