2c8d1ebadc5b80610db2bdc5cc3cefe420ca321d
machamp0714 <rateliasudaisuki@gmail.com>
2020-11-02 23:29:04 +0900

    cloud functionsを試す

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import { collectionName } from './services/constants';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const todos = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.todos)
+      .get();
+
+    const data = snap.docs.map(doc => doc.data());
+    res.send({ data })
+  })
